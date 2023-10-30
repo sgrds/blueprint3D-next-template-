e4ac07e154c4d21747107dd1c3648a0cd6d500ef
sgrDs <sagardasdev@gmail.com>
2023-10-30 09:19:44 +0530

    imported the script of blueprint3D library and the example

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,265 @@
-import Image from 'next/image'
+"use client";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="container-fluid">
+      <div className="row main-row">
+        {/* <!-- Left Column --> */}
+        <div className="col-xs-3 sidebar">
+          {/* <!-- Main Navigation --> */}
+          <ul className="nav nav-sidebar">
+            <li id="floorplan_tab">
+              <a href="#">
+                Edit Floorplan
+                <span className="glyphicon glyphicon-chevron-right pull-right"></span>
+              </a>
+            </li>
+            <li id="design_tab">
+              <a href="#">
+                Design
+                <span className="glyphicon glyphicon-chevron-right pull-right"></span>
+              </a>
+            </li>
+            <li id="items_tab">
+              <a href="#">
+                Add Items
+                <span className="glyphicon glyphicon-chevron-right pull-right"></span>
+              </a>
+            </li>
+          </ul>
+          <hr />
+
+          {/* <!-- Context Menu --> */}
+          <div id="context-menu">
+            <div>
+              <span id="context-menu-name" className="lead"></span>
+              <br />
+              <br />
+              <button
+                className="btn btn-block btn-danger"
+                id="context-menu-delete"
+              >
+                <span className="glyphicon glyphicon-trash"></span>
+                Delete Item
+              </button>
+              <br />
+              <div className="panel panel-default">
+                <div className="panel-heading">Adjust Size</div>
+                <div className="panel-body">
+                  <div
+                    className="form form-horizontal"
+                    // className="lead"
+                  >
+                    <div className="form-group">
+                      <label className="col-sm-5 control-label">Width</label>
+                      <div className="col-sm-6">
+                        <input
+                          type="number"
+                          className="form-control"
+                          id="item-width"
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <label className="col-sm-5 control-label">Depth</label>
+                      <div className="col-sm-6">
+                        <input
+                          type="number"
+                          className="form-control"
+                          id="item-depth"
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <label className="col-sm-5 control-label">Height</label>
+                      <div className="col-sm-6">
+                        <input
+                          type="number"
+                          className="form-control"
+                          id="item-height"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <small>
+                    <span className="text-muted">Measurements in inches.</span>
+                  </small>
+                </div>
+              </div>
+
+              <label>
+                <input type="checkbox" id="fixed" /> Lock in place
+              </label>
+              <br />
+              <br />
+            </div>
+          </div>
+
+          {/* <!-- Floor textures --> */}
+          <div id="floorTexturesDiv">
+            <div className="panel panel-default">
+              <div className="panel-heading">Adjust Floor</div>
+              <div className="panel-body">
+                <div className="col-sm-6">
+                  <a
+                    href="#"
+                    className="thumbnail texture-select-thumbnail"
+                    texture-url="rooms/textures/light_fine_wood.jpg"
+                    texture-stretch="false"
+                    texture-scale="300"
+                  >
+                    <img
+                      alt="Thumbnail light fine wood"
+                      src="rooms/thumbnails/thumbnail_light_fine_wood.jpg"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* <!-- Wall Textures --> */}
+          <div id="wallTextures">
+            <div className="panel panel-default">
+              <div className="panel-heading">Adjust Wall</div>
+              <div className="panel-body">
+                <div className="col-sm-6">
+                  <a
+                    href="#"
+                    className="thumbnail texture-select-thumbnail"
+                    texture-url="rooms/textures/marbletiles.jpg"
+                    texture-stretch="false"
+                    texture-scale="300"
+                  >
+                    <img
+                      alt="Thumbnail marbletiles"
+                      src="rooms/thumbnails/thumbnail_marbletiles.jpg"
+                    />
+                  </a>
+                </div>
+                <div className="col-sm-6">
+                  <a
+                    href="#"
+                    className="thumbnail texture-select-thumbnail"
+                    texture-url="rooms/textures/wallmap_yellow.png"
+                    texture-stretch="true"
+                    texture-scale=""
+                  >
+                    <img
+                      alt="Thumbnail wallmap yellow"
+                      src="rooms/thumbnails/thumbnail_wallmap_yellow.png"
+                    />
+                  </a>
+                </div>
+                <div className="col-sm-6">
+                  <a
+                    href="#"
+                    className="thumbnail texture-select-thumbnail"
+                    texture-url="rooms/textures/light_brick.jpg"
+                    texture-stretch="false"
+                    texture-scale="100"
+                  >
+                    <img
+                      alt="Thumbnail light brick"
+                      src="rooms/thumbnails/thumbnail_light_brick.jpg"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+
+        {/* <!-- Right Column --> */}
+        <div className="col-xs-9 main">
+          {/* <!-- 3D Viewer --> */}
+          <div id="viewer">
+            <div id="main-controls">
+              <a href="#" className="btn btn-default btn-sm" id="new">
+                New Plan
+              </a>
+              <a href="#" className="btn btn-default btn-sm" id="saveFile">
+                Save Plan
+              </a>
+              <a className="btn btn-sm btn-default btn-file">
+                <input type="file" className="hidden-input" id="loadFile" />
+                Load Plan
+              </a>
+            </div>
+
+            <div id="camera-controls">
+              <a href="#" className="btn btn-default bottom" id="zoom-out">
+                <span className="glyphicon glyphicon-zoom-out"></span>
+              </a>
+              <a href="#" className="btn btn-default bottom" id="reset-view">
+                <span className="glyphicon glyphicon glyphicon-home"></span>
+              </a>
+              <a href="#" className="btn btn-default bottom" id="zoom-in">
+                <span className="glyphicon glyphicon-zoom-in"></span>
+              </a>
+
+              <span>&nbsp;</span>
+
+              <a className="btn btn-default bottom" href="#" id="move-left">
+                <span className="glyphicon glyphicon-arrow-left"></span>
+              </a>
+              <span className="btn-group-vertical">
+                <a className="btn btn-default" href="#" id="move-up">
+                  <span className="glyphicon glyphicon-arrow-up"></span>
+                </a>
+                <a className="btn btn-default" href="#" id="move-down">
+                  <span className="glyphicon glyphicon-arrow-down"></span>
+                </a>
+              </span>
+              <a className="btn btn-default bottom" href="#" id="move-right">
+                <span className="glyphicon glyphicon-arrow-right"></span>
+              </a>
+            </div>
+
+            <div id="loading-modal">
+              <h1>Loading...</h1>
+            </div>
+          </div>
+
+          {/* <!-- 2D Floorplanner --> */}
+          <div id="floorplanner">
+            <canvas id="floorplanner-canvas"></canvas>
+            <div id="floorplanner-controls">
+              <button id="move" className="btn btn-sm btn-default">
+                <span className="glyphicon glyphicon-move"></span>
+                Move Walls
+              </button>
+              <button id="draw" className="btn btn-sm btn-default">
+                <span className="glyphicon glyphicon-pencil"></span>
+                Draw Walls
+              </button>
+              <button id="delete" className="btn btn-sm btn-default">
+                <span className="glyphicon glyphicon-remove"></span>
+                Delete Walls
+              </button>
+              <span className="pull-right">
+                <button
+                  className="btn btn-primary btn-sm"
+                  id="update-floorplan"
+                >
+                  Done &raquo;
+                </button>
+              </span>
+            </div>
+            <div id="draw-walls-hint">
+              {`Press the "Esc" key to stop drawing walls`}
+            </div>
+          </div>
+
+          {/* <!-- Add Items --> */}
+          <div id="add-items">
+            <div className="row" id="items-wrapper">
+              {/* <!-- Items added here by items.js --> */}
+            </div>
+          </div>
+        </div>
+        {/* <!-- End Right Column --> */}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
